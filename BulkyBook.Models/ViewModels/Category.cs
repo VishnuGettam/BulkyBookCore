d6450770ab94f3820bcc48cc9c4f307de05c3e6f
VishnuGettam <vishnugettam@gmail.com>
2021-08-25 13:52:16 +0530

    Created Identity tables and Category .

---
+++ BulkyBook.Models/ViewModels/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models.ViewModels
+{
+    [Table("tblCategory")]
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+    }
+}
